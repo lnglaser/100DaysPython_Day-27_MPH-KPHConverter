b6c53d1afefb77fc03c3831f8b8d4735da18b36f
Leon Glaser <leon.glaser@gmail.com>
2023-09-15 17:37:12 -0400

    MVP reached

--- main.py
+++ main.py
@@ -6,5 +6,30 @@ window.minsize(width=400, height=200)
 window.eval("tk::PlaceWindow . center")
 window.config(padx=20, pady=20)
 
-window.mainloop()
 
+def convert():
+    kilometers = float(user_input.get()) * 1.609
+    converted_amount.config(text=kilometers)
+
+
+# Add placeholder text later
+user_input = Entry(width=5)
+user_input.grid(column=1, row=0)
+
+convert_from = Label(text="Miles")
+convert_from.grid(column=2, row=0)
+
+is_equal_to = Label(text="is equal to")
+is_equal_to.grid(column=0, row=1)
+
+converted_amount = Label(text=0)
+converted_amount.grid(column=1, row=1)
+
+convert_to = Label(text="Km")
+convert_to.grid(column=2, row=1)
+
+calculate = Button(text="Calculate", command=convert)
+calculate.grid(column=1, row=2)
+
+
+window.mainloop()
