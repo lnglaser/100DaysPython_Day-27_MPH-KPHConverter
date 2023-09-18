085304eb9012a637cd7be426fe11b7efdcf907a4
Leon Glaser <leon.glaser@gmail.com>
2023-09-18 12:14:33 -0400

    add error catch for non-numeric input

--- main.py
+++ main.py
@@ -2,14 +2,19 @@ from tkinter import *
 
 window = Tk()
 window.title("Distance converter")
-window.minsize(width=300, height=150)
+window.minsize(width=400, height=150)
 window.eval("tk::PlaceWindow . center")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=20)
 
 
 def convert():
-    kilometers = float(user_input.get()) * 1.609
-    converted_amount.config(text=kilometers)
+    miles_value = user_input.get()
+    if miles_value.isnumeric():
+        kilometers = float(miles_value) * 1.609
+        converted_amount.config(text=kilometers)
+        error_message.config(text="")
+    else:
+        error_message.config(text="Please enter a number")
 
 
 # Add placeholder text later
@@ -17,7 +22,6 @@ user_input = Entry(width=10)
 user_input.grid(column=1, row=0)
 user_input.insert(0, "Enter # of miles")
 user_input.focus()
-
 
 convert_from = Label(text="Miles")
 convert_from.grid(column=2, row=0)
@@ -38,5 +42,8 @@ convert_to.config(padx=10, pady=10)
 calculate = Button(text="Calculate", command=convert)
 calculate.grid(column=1, row=2)
 
+error_message = Label()
+error_message.grid(column=1, row=4)
+error_message.config(padx=5, pady=5)
 
 window.mainloop()
